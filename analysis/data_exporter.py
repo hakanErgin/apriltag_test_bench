c800aada684ec453ad2429a264614e1c9cc551a9
hakanErgin <hakan.ergin@gmail.com>
2022-03-24 15:30:57 +0100

    linear velocity exported

--- analysis/data_exporter.py
+++ analysis/data_exporter.py
@@ -3,15 +3,22 @@ import nbformat
 
 
 def display_category(paths, notebooks_type):
+    if notebooks_type == "translations" or notebooks_type == "rotations":
+        for nb_path in paths[notebooks_type]:
+            nb_cells = nbformat.read(nb_path, as_version=4)
+            error_description, error_plot, nb_name = export_data(
+                nb_cells, notebooks_type, nb_path
+            )
+            display(nb_name)
+            display(error_description)
+            display_png(error_plot, raw=True)
 
-    for nb_path in paths[notebooks_type]:
-        nb_cells = nbformat.read(nb_path, as_version=4)
-        error_description, error_plot, nb_name = export_data(
-            nb_cells, notebooks_type, nb_path
-        )
-        display(nb_name)
-        display(error_description)
-        display_png(error_plot, raw=True)
+    if notebooks_type == "linear_velocity" or notebooks_type == "angular_velocity":
+        for nb_path in paths[notebooks_type]:
+            nb_cells = nbformat.read(nb_path, as_version=4)
+            detection_ratios, nb_name = export_data(nb_cells, notebooks_type, nb_path)
+            display(nb_name)
+            print(detection_ratios)
 
 
 def export_data(nb, notebooks_type, nb_path):
@@ -24,8 +31,17 @@ def export_data(nb, notebooks_type, nb_path):
                 error_description = HTML(cell.outputs[0].data["text/html"])
             if cell.source == "box_plotter('error')":
                 error_plot = cell.outputs[0].data["image/png"]
+        return (error_description, error_plot, get_nb_name(nb_path, notebooks_type))
 
-    return (error_description, error_plot, get_nb_name(nb_path, notebooks_type))
+    if notebooks_type == "linear_velocity" or notebooks_type == "angular_velocity":
+        for cell in nb.cells:
+            if (
+                cell.source
+                == "# ratio of detected to not\nratios = df.is_detected.value_counts(normalize=True)*100\nratios"
+            ):
+                return cell.outputs[0].data["text/plain"], get_nb_name(
+                    nb_path, notebooks_type
+                )
 
 
 def get_nb_name(nb_path, notebooks_type):
@@ -39,4 +55,11 @@ def get_nb_name(nb_path, notebooks_type):
         res = nb_path.split("_")[-2]
         dist = nb_path.split("_")[-3]
         tag = nb_path.split("_")[-4]
+        return str(tag + " - " + dist + " - " + res)
+
+    elif notebooks_type == "linear_velocity":
+        res = nb_path.split("_")[-1].split(".")[-2]
+        dist = nb_path.split("_")[-4] + "." + nb_path.split("_")[-3]
+        dist = dist.split("/")[-1]
+        tag = nb_path.split("_")[-2]
         return str(tag + " - " + dist + " - " + res)
